425b4e7c4ef5b6603540c9af6e4b034b69b52453
sirdeya <allan.sirdey@cgi.com>
2018-02-22 13:05:18 +0100

    modify layout activity Apply offer

--- app/src/main/java/com/cgi/formation/monstaffing/MainActivity.java
+++ app/src/main/java/com/cgi/formation/monstaffing/MainActivity.java
@@ -1,13 +1,28 @@
 package com.cgi.formation.monstaffing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.cgi.formation.monstaffing.activities.ApplyOfferActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.button_applyToOffer:
+                Intent intent = new Intent(this, ApplyOfferActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
