5fb7f147ea76c1c70343dc07480fe1dec56af9c7
AxelRenard <33868676+AxelRenard@users.noreply.github.com>
2018-02-22 15:50:59 +0100

    Scroller sur le filtre

--- app/src/main/java/com/cgi/formation/monstaffing/activities/FiltreActivity.java
+++ app/src/main/java/com/cgi/formation/monstaffing/activities/FiltreActivity.java
@@ -1,8 +1,11 @@
 package com.cgi.formation.monstaffing.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -11,16 +14,34 @@ import com.cgi.formation.monstaffing.models.Mission;
 
 public class FiltreActivity extends AppCompatActivity {
 
+    public static final String BUNDLE_MOT_CLE = "BUNDLE_MOT_CLE";
+    public static final String BUNDLE_VILLE = "BUNDLE_VILLE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtre);
 
-        EditText motCleRentre = findViewById(R.id.motCleUser);
-        Spinner villeChoisie = findViewById(R.id.villeChoisie);
+        final EditText motCleRentre = findViewById(R.id.motCleUser);
+        final Spinner villeChoisie = findViewById(R.id.villeChoisie);
+        Button boutonValidation = findViewById(R.id.boutonValidation);
 
         villeChoisie.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Mission.Lieu.values()));
 
+
+        boutonValidation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                String motChoisi = motCleRentre.getText().toString();
+                intent.putExtra(BUNDLE_MOT_CLE, motChoisi);
+                Mission.Lieu lieuChoisie = (Mission.Lieu) villeChoisie.getSelectedItem();
+                intent.putExtra(BUNDLE_VILLE, lieuChoisie.getValue());
+                setResult(RESULT_OK, intent);
+                finish();
+
+            }
+        });
 
 
     }
