f416c6e2567442176726e51c772510690c2a9aee
AxelRenard <33868676+AxelRenard@users.noreply.github.com>
2018-02-23 15:56:56 +0100

    Ajout du rafraichissement sur le XML

--- app/src/main/java/com/cgi/formation/monstaffing/activities/FiltreActivity.java
+++ app/src/main/java/com/cgi/formation/monstaffing/activities/FiltreActivity.java
@@ -1,16 +1,14 @@
 package com.cgi.formation.monstaffing.activities;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
+
 
 import com.cgi.formation.monstaffing.R;
 import com.cgi.formation.monstaffing.models.Mission;
